478e05801d41697fe70d45f550c9262940b6a19e
Camden Phalen <camden@Camdens-MacBook-Pro.local>
2020-09-04 16:49:10 -0700

    more setup

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <link rel="stylesheet" href="https://use.typekit.net/kez3dur.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
