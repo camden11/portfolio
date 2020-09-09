979996b8eeb442e4a977c4d20a262e5c8f424c45
Camden Phalen <camden@Camdens-MacBook-Pro.local>
2020-09-09 12:02:31 -0700

    images

--- util/image.js
+++ util/image.js
@@ -2,6 +2,9 @@ export const formatImagesForGallery = (images, alt) => {
   return images.map((image) => {
     return {
       src: image,
+      thumbnail: "",
+      thumbnailWidth: 0,
+      thumbnailHeight: 0,
       alt,
     };
   });
